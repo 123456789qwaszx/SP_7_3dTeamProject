60251bf8f2692d84f8fd69a34791d9464949a86d
pjhzzazz <pjhzzazzz@naver.com>
2025-05-27 14:27:39 +0900

    Install AI Navigation / Add MonsterData / Add MonsterBase

---
+++ Assets/95.JiHwan/Scripts/Monster/Bear.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+//using UnityEngine.InputSystem.Android;
+using Random = UnityEngine.Random;
+
+
+public class Bear : MonsterBase//, IDamageable
+{
+    
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+       
+    }
+
+  
+
+    
+}
+
+
+
