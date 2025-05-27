bb727240ac804d9c2a51b5bb376d029453cc20a0
kimhhjj <gksdnf1984@naver.com>
2025-05-27 20:10:15 +0900

    추가 : 무한점프 수정 후 어택 추가
    그라운드 인식 추가해서 무한 점프 수정
    어택 모션 추가

--- Assets/01.Scripts/Player/PlayerController.cs
+++ Assets/01.Scripts/Player/PlayerController.cs
@@ -4,14 +4,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class PlayerController : MonoBehaviour
 {
   
     private Rigidbody _rigidbody;
     private Vector2 _playerInput;
+    Animator animator;
+    
+    
     [SerializeField] private  PlayerStats playerStats;
+    
+    [Header("땅(그라운드) 체크")]
     [SerializeField] private bool IsGround;
     [SerializeField] private Transform groundCheckTr;
+    [SerializeField] private float groundDistance = 0.3f;
+    [SerializeField] private LayerMask groundMask;
+    
+    [Header("카메라")]
+    [SerializeField] private Transform cameraContainer;
+    [SerializeField] private Transform camTr;
+    private Vector2 lookInput;
+    private float xRotation = 0f;
+    public float mouseSensitivity = 1f;
+
     
     
     void Start()
@@ -22,13 +38,16 @@ public class PlayerController : MonoBehaviour
     void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
 
 
     void Update()
     {
+        CheckGround();
         HandleLook();
+       
     }
 
     void FixedUpdate()
@@ -36,10 +55,29 @@ public class PlayerController : MonoBehaviour
         HandleMove();
     }
 
+    public void CheckGround()
+    {
+        IsGround = Physics.CheckSphere(groundCheckTr.position, groundDistance, groundMask);
+        if (!IsGround)
+        {
+            animator.SetBool("IsJump", false);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (groundCheckTr != null)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(groundCheckTr.position, groundDistance);
+        }
+    }
+
     public void Move()
     {
         Vector3 move = new Vector3(_playerInput.x, 0, _playerInput.y);
         _rigidbody.MovePosition(this.transform.position + move *playerStats.Speed  * Time.deltaTime);
+        
 
     }
 
@@ -48,6 +86,7 @@ public class PlayerController : MonoBehaviour
         if (context.performed)
         {
             _playerInput = context.ReadValue<Vector2>();
+            Debug.Log($"Cheak{_playerInput}");
         }
         else
         {
@@ -57,18 +96,24 @@ public class PlayerController : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        Debug.Log("Jump입력 됨");
+        if (context.performed && IsGround)
         {
-            _rigidbody.AddForce(Vector3.up * playerStats.JumpForce, ForceMode.Impulse);
+            Debug.Log("true로 못 들어오는 중");
+            
+            animator.SetBool("IsJump", true);
+            
         }
+        Debug.Log("현재 땅이 아닙니다.");
+    }
+
+    public void AddJump()
+    {
+        _rigidbody.AddForce(Vector3.up * playerStats.JumpForce, ForceMode.Impulse);
+       
     }
     
-    [SerializeField] private Transform cameraContainer;
-    [SerializeField] private Transform camTr;
-    private Vector2 lookInput;
-    private float xRotation = 0f;
-    public float mouseSensitivity = 1f;
-
+ 
 
     public void OnLook(InputAction.CallbackContext context)
     {
@@ -88,6 +133,16 @@ public class PlayerController : MonoBehaviour
         transform.Rotate(Vector3.up * mouseDelta.x);
     }
 
+    public void OnAttacking(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("마우스 좌클릭");
+            animator.SetTrigger("IsAttack");
+        }
+        Debug.Log("마우스 좌클릭이 안됨");
+    }
+
     private void HandleMove()
     {
         Vector3 camForwad = camTr.forward;
@@ -100,9 +155,14 @@ public class PlayerController : MonoBehaviour
         camRight.Normalize();
 
         Vector3 move = camRight * _playerInput.x + camForwad * _playerInput.y;
+        
+        bool isMoving = move.sqrMagnitude > 0.01f;
+        animator.SetBool("IsRun", isMoving);
+        
         Vector3 velocity = new Vector3(move.x * playerStats.Speed, _rigidbody.velocity.y, move.z * playerStats.Speed);
         
         _rigidbody.velocity = velocity;
+
     }
 
 
