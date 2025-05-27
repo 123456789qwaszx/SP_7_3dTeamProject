e25b7b617e9f0225faeb4c8154e3f5b64bb0c0ad
whomery <whomery12345@gmail.com>
2025-05-27 10:17:20 +0900

    NPC
    dd

---
+++ Assets/01.Scripts/NPC/NPCManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NPCManager : MonoBehaviour
+{
+    public NPC Apple;
+
+    // Start is called before the first frame update
+
+}
