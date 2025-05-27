ea68349287e97feb8299ceaa4036e2abd04cf6c6
YuJHun <76084438+YuJHun@users.noreply.github.com>
2025-05-27 20:09:33 +0900

    yjh

--- Assets/01.Scripts/UI/New Folder/UIPlayerStats.cs
+++ Assets/01.Scripts/UI/New Folder/UIPlayerStats.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
 
 public class UIPlayerStats : MonoBehaviour
 {
@@ -12,7 +13,7 @@ public class UIPlayerStats : MonoBehaviour
     Condition hunger { get { return uiCondition.hunger; } }
     private float playerHealth;
     public float noHydrationHealthDecay;
-    public float noHungerHealthDecay;
+    public float noHungerHealthDecay = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,27 +21,39 @@ public class UIPlayerStats : MonoBehaviour
     }
     void Awake()
     {
-        //playerHealth = Managers.Player.Player.PlayerStats.Health;
     }
     // Update is called once per frame
     void Update()
     {
-        hydration.Add(hydration.passiveValue * Time.deltaTime);
+        hunger.Subtract(noHungerHealthDecay * Time.deltaTime);
         hunger.Add(hunger.passiveValue * Time.deltaTime);
+        hydration.Subtract(hydration.passiveValue * Time.deltaTime);
 
-        if (hydration.curValue == 0)
+        if (hydration.curValue <= 0f)
         {
             health.Subtract(noHydrationHealthDecay * Time.deltaTime);
         }
-        if (hunger.curValue == 0)
+        if (hunger.curValue <= 0f)
         {
             health.Subtract(noHungerHealthDecay * Time.deltaTime);
         }
-        if (health.curValue == 0)
+        if (health.curValue <= 0f)
         {
             Die();
         }
 
+    }
+    public void Heal(float amount)
+    {
+        health.Add(amount);
+    }
+    public void Drink(float amount)
+    {
+        hydration.Add(amount);
+    }
+    public void Eat(float amount)
+    {
+        hunger.Add(amount);
     }
     public void Die()
     {
