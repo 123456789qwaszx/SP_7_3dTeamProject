c4cabca291a2cef3098278884383a6b591701e04
whomery <whomery12345@gmail.com>
2025-05-28 19:39:59 +0900

    NPC 상호작용 대사 추가

--- Assets/01.Scripts/NPC/NPC.cs
+++ Assets/01.Scripts/NPC/NPC.cs
@@ -2,41 +2,60 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPC : MonoBehaviour
 {
     public GameObject MarkNPCName;
     public GameObject NPCSearch;
-
+    public GameObject NPCManager;
+    public GameObject NPCTextBoard;
 
 
     private void OnTriggerEnter(Collider other)
     {
         string npcName = this.gameObject.name;
+
+        //플레이어 테그붙은 오브젝트가 닿은경우
+
         if (other.CompareTag("Player"))
         {
- 
-                // NPCSearch 오브젝트를 MarkNPCName의 자식으로 설정
-                // TextMeshPro 컴포넌트를 찾아 이름 설정
-                Instantiate(NPCSearch, MarkNPCName. transform).GetComponentInChildren<TextMeshProUGUI>().text = npcName;
-                    NPCSearch.gameObject.name = npcName;
+            // NPCSearch 오브젝트 생성 및 부모 지정
 
-            
+            GameObject instance = Instantiate(NPCSearch, MarkNPCName.transform);
+
+            // 텍스트 설정
+
+            instance.GetComponentInChildren<TextMeshProUGUI>().text = npcName;
+
+            // 이름 지정
+
+            instance.name = npcName;
+
+            // 버튼 클릭 이벤트 연결
+
+             instance.GetComponent<Button>().onClick.AddListener(NPCManager.GetComponent<SearchNPCNameBtn>().ButtonClick);
+                
         }
     }
 
-    private void OnTriggerExit(Collider other)
+
+
+    private void OnTriggerExit(Collider other)//멀어질 경우 해당 생성 오브젝트 삭제
     {
+        
+        string npcName = this.gameObject.name;
+
+       
+
         if (other.CompareTag("Player"))
-        {
-            string npcName = this.gameObject.name;
-
-            // 자식 중에 npcName을 가진 오브젝트를 찾아서 제거
-
-            Destroy(MarkNPCName.transform.Find(npcName + "(Clone)").gameObject);
-
-
-
+        {         
+            Destroy(MarkNPCName.transform.Find(npcName).gameObject);
         }
+
+        NPCTextBoard.SetActive(false);
     }
+
+
+
 }
