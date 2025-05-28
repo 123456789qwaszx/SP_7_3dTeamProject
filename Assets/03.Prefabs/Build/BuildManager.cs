18c65fc58e59cb09a988f59ce36d9b3933c70b27
YeonDarm <msangyun29@naver.com>
2025-05-28 13:08:41 +0900

    #13_BuildManual, BuildManager 기능 분리(테스트 중)
    UI가 비활성 되지 않는 버그(수정 중)

--- Assets/03.Prefabs/Build/BuildManager.cs
+++ Assets/03.Prefabs/Build/BuildManager.cs
@@ -1,18 +1,129 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
+// 미리보기 및 생성 
 public class BuildManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static BuildManager Instance { get; private set; }
+
+    //미리보기 프리팹 상태 시 취소 이벤트.
+    public event Action OnCancelBuild;
+
+    public bool isPreviewActivated = false; // 미리보기 상태
+    private GameObject go_Preview; // 미리보기 프리팹을 담을 변수
+    private GameObject go_Prefab; // 실제 생성 프리팹을 담을 변수
+    [SerializeField] private Transform tf_PlayerCam; // Player Camera 위치로 생성
+
+    // Ray로 설치 지점을 지정.
+    private RaycastHit hitInfo;
+    [SerializeField] private LayerMask layerMask; // 설치 가능or불가능 필터링할 레이어
+    [SerializeField] private float rayRange;
+
+    private void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (isPreviewActivated)
+        {
+            PreviewPosUpdate();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && isPreviewActivated)
+        {
+            Debug.Log("마우스 클릭: 건설 시작");
+            Build();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cancel();
+        }
+    }
+
+    public void SelectedBuilding(Building selectedBuilding)
+    {
+        go_Preview = Instantiate(
+            selectedBuilding.go_PreviewPrefabs,
+            tf_PlayerCam.position + tf_PlayerCam.forward,
+            Quaternion.identity
+        );
+
+        go_Prefab = selectedBuilding.go_Prefabs; // 테스트용
+        isPreviewActivated = true;
         
+        //SO 전용
+        // if (_slotNumber < buildList.Length)
+        // {
+        //     BuildData selectedBuilding = buildList[_slotNumber];
+
+        //     go_Preview = Instantiate(
+        //         selectedBuilding.previewPrefab,
+        //         tf_Player.position + tf_Player.forward,
+        //         Quaternion.identity
+        //     );
+        // }
+
+        // isPreviewActivated = true; // 미리보기 활성화 상태
+        // go_BaseUI.SetActive(false); // 제작탭 종료
+    }
+    private void Build()
+    {
+        Quaternion lockRot = Quaternion.Euler(0, tf_PlayerCam.rotation.eulerAngles.y, 0); // 건축물 회전 잠금
+
+        //미리보기 활성화 되고 미리보기 오브젝트가 설치 가능한 상태일 때
+        if (isPreviewActivated && go_Preview.GetComponent<PreviewObject>().IsBuildable())
+        {
+            Debug.Log("건축 완료");
+
+            Instantiate(go_Prefab, hitInfo.point, lockRot);
+            Destroy(go_Preview);
+
+            // isActivated = false;
+            isPreviewActivated = false;
+            go_Preview = null;
+            go_Prefab = null;
+        }
+        else
+        {
+            Debug.Log("건축 불가: 장애물 확인");
+        }
+    }
+
+    private void PreviewPosUpdate() // 
+    {
+        if (Physics.Raycast(tf_PlayerCam.position, tf_PlayerCam.forward, out hitInfo, rayRange, layerMask))
+        {
+
+            if (hitInfo.transform != null)
+            {
+                Vector3 _location = hitInfo.point;
+                go_Preview.transform.position = _location;
+                go_Preview.transform.rotation = Quaternion.Euler(0, tf_PlayerCam.rotation.eulerAngles.y, 0); // 미리보기 회전 잠금
+            }
+        }
+    }
+
+    private void Cancel() // 미리보기 상태 취소
+    {
+        if (isPreviewActivated)
+        {
+            Destroy(go_Preview);
+        }
+
+        isPreviewActivated = false;
+        go_Preview = null;
+        go_Prefab = null;
+
+        OnCancelBuild?.Invoke();
     }
 }
--- Assets/03.Prefabs/Build/BuildManual.cs
+++ Assets/03.Prefabs/Build/BuildManual.cs
@@ -12,38 +12,41 @@ public class Building
 
 public class BuildManual : MonoBehaviour
 {
-    private bool isActivated = false; //
-    
     [SerializeField] private GameObject go_BaseUI; // 건축 UI
     [SerializeField] private Building[] builds; // 설치물 저장
+    private bool isActivated = false; // UI 활성 상태
 
     // [SerializeField] private BuildData[] buildList; // 건축물(SO) 리스트
     // go_Preview, go_Prefab, tf_PlayerCam, isPreviewActivated
-    
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab) && !BuildManager.Instance.isPreviewActivated)
+        {
+            Window();
+            Debug.Log("Tab키 작동");
+        }
+    }
+
     public void SlotClick(int _slotNumber)
     {
-        go_Preview = Instantiate(
-            builds[_slotNumber].go_PreviewPrefabs,
-            tf_PlayerCam.position + tf_PlayerCam.forward,
-            Quaternion.identity
-        );
+        BuildManager.Instance.SelectedBuilding(builds[_slotNumber]);
+    }
 
-        go_Prefab = builds[_slotNumber].go_Prefabs; // 테스트용
+    private void OnEnable()
+    {
+        if (BuildManager.Instance != null)
+        {
+            BuildManager.Instance.OnCancelBuild += CloseWindow;
+        }
+    }
 
-        //SO 전용
-        // if (_slotNumber < buildList.Length)
-        // {
-        //     BuildData selectedBuilding = buildList[_slotNumber];
-
-        //     go_Preview = Instantiate(
-        //         selectedBuilding.previewPrefab,
-        //         tf_Player.position + tf_Player.forward,
-        //         Quaternion.identity
-        //     );
-        // }
-
-        isPreviewActivated = true; // 미리보기 활성화 상태
-        go_BaseUI.SetActive(false); // 제작탭 종료
+    private void OnDisable()
+    {
+        if (BuildManager.Instance != null)
+        {
+            BuildManager.Instance.OnCancelBuild -= CloseWindow;
+        }
     }
 
     private void Window()
@@ -69,5 +72,4 @@ public class BuildManual : MonoBehaviour
         isActivated = false;
         go_BaseUI.SetActive(false);
     }
-
 }
