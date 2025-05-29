9e9a5fd7bbd9a582e72d528a9446494580d17fd8
kimhhjj <gksdnf1984@naver.com>
2025-05-29 20:13:05 +0900

    추가 : 플레이어 방향 이동 및 DIe 플레이어컨트롤러로 이동
    플레이어 방향 추가 및
    Die 스크립트를 플레이어컨트롤.cs로 옮김

--- Assets/01.Scripts/Player/PlayerStats.cs
+++ Assets/01.Scripts/Player/PlayerStats.cs
@@ -5,12 +5,12 @@ using UnityEngine.InputSystem.EnhancedTouch;
 
 public class PlayerStats : MonoBehaviour
 {
-    public UIOpenClose uiOpenClose;
+    // public UIOpenClose uiOpenClose;
     public float noHungerHealthDecay = 5f;
     public UIStat uiStat;
     [SerializeField]
     private Condition _health;
-    public Condition Health // 체력
+    public Condition Health             // 체력
     {
         get { return _health; }
         set { _health = value; }
@@ -25,7 +25,7 @@ public class PlayerStats : MonoBehaviour
     [SerializeField]
     private Condition _hydration;
 
-    public Condition Hydration          // 수분
+    public Condition Hydration           // 수분
     {
         get { return _hydration; }
         set { _hydration = value; }
@@ -33,14 +33,14 @@ public class PlayerStats : MonoBehaviour
     [SerializeField]
     private float _attack;
 
-    public float Attack             // 공격력
+    public float Attack                  // 공격력
     {
         get { return _attack;}
         set { _attack = value; }
     }
     [SerializeField]
     private float _armor;
-    public float Armor              // 방어력
+    public float Armor                   // 방어력
     {
         get {return _armor; }
         set { _armor = value; }
@@ -48,15 +48,15 @@ public class PlayerStats : MonoBehaviour
     [SerializeField]
     private float _speed;
 
-    public float Speed
+    public float Speed                   // 스피드
     {
         get {return _speed; }
         set { _speed = value; }
     }
     [SerializeField]
-    private float _jumpFoce;
+    private float _jumpFoce;             
 
-    public float JumpForce
+    public float JumpForce               // 점프
     {
         get {return _jumpFoce; }
         set { _jumpFoce = value; }
@@ -70,46 +70,46 @@ public class PlayerStats : MonoBehaviour
     void Update()
     {
 
-        //Hunger.Subtract(noHungerHealthDecay * Time.deltaTime);
-        Hunger.Subtract(Hunger.passiveValue * Time.deltaTime);
-        Hydration.Subtract(Hydration.passiveValue * Time.deltaTime);
+        // //Hunger.Subtract(noHungerHealthDecay * Time.deltaTime);
+        // Hunger.Subtract(Hunger.passiveValue * Time.deltaTime);
+        // Hydration.Subtract(Hydration.passiveValue * Time.deltaTime);
+        //
+        // if (Hydration.curValue <= 0f)
+        // {
+        //     Health.Subtract(noHungerHealthDecay * Time.deltaTime);
+        // }
+        // if (Hunger.curValue <= 0f)
+        // {
+        //     Health.Subtract(noHungerHealthDecay * Time.deltaTime);
+        // }
+        // if (Health.curValue <= 0f)
+        // {
+        //     Die();
+        // }
 
-        if (Hydration.curValue <= 0f)
-        {
-            Health.Subtract(noHungerHealthDecay * Time.deltaTime);
-        }
-        if (Hunger.curValue <= 0f)
-        {
-            Health.Subtract(noHungerHealthDecay * Time.deltaTime);
-        }
-        if (Health.curValue <= 0f)
-        {
-            Die();
-        }
-
     }
-    public void Heal(float amount)
-    {
-        Health.Add(amount);
-    }
-    public void Drink(float amount)
-    {
-        Hydration.Add(amount);
-    }
-    public void Eat(float amount)
-    {
-        Health.Add(amount);
-    }
-    public void Die()
-    {
-        Debug.Log("died.");
-        if (uiOpenClose != null)
-        {
-            uiOpenClose.OCGameOver();
-        }
-        else
-        {
-            Debug.LogWarning("UIOpenClose가 연결되지 않았습니다.");
-        }
-    }
+    // public void Heal(float amount)
+    // {
+    //     Health.Add(amount);
+    // }
+    // public void Drink(float amount)
+    // {
+    //     Hydration.Add(amount);
+    // }
+    // public void Eat(float amount)
+    // {
+    //     Health.Add(amount);
+    // }
+    // public void Die()
+    // {
+    //     Debug.Log("died.");
+    //     if (uiOpenClose != null)
+    //     {
+    //         uiOpenClose.OCGameOver();
+    //     }
+    //     else
+    //     {
+    //         Debug.LogWarning("UIOpenClose가 연결되지 않았습니다.");
+    //     }
+    // }
 }
