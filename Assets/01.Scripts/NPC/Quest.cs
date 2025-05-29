9ad8a3a9907015734801b927c094eb300b7d798c
whomery <whomery12345@gmail.com>
2025-05-29 20:01:54 +0900

    대사 스크립트
    애니 추가

--- Assets/01.Scripts/NPC/Quest.cs
+++ Assets/01.Scripts/NPC/Quest.cs
@@ -1,14 +1,36 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Quest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+   
+    private bool[] QC = new bool[10];
+    public int i = 0;
+
+    public void QuestCondition1() //Q1
+    { 
+        if(i < 1)
+        {
+            QC[0] = true;
+        }
+        else
+        {
+            return;
+        }
     }
+    public void QuestCondition2() //Q2
+    {
+        if (i < 2)
+        {
+            QC[1] = true;
+        }
+        else
+        {
+            return;
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
--- Assets/01.Scripts/NPC/SearchNPCNameBtn.cs
+++ Assets/01.Scripts/NPC/SearchNPCNameBtn.cs
@@ -13,8 +13,11 @@ public class SearchNPCNameBtn : MonoBehaviour
     public GameObject NPCText;
     public GameObject NPCName;
 
+
     public void ButtonClick()
     {
+       
+
         // 텍스트 보드 활성화
         NPCTextBoard.SetActive(true);
 
@@ -27,6 +30,10 @@ public class SearchNPCNameBtn : MonoBehaviour
         string clickedName = EventSystem.current.currentSelectedGameObject.name;
         NPCText.GetComponentInChildren<TextMeshProUGUI>().text = "안녕?";
         NPCName.GetComponentInChildren<TextMeshProUGUI>().text = clickedName;
+        Button btn = NPCTextBoard.GetComponentInChildren<Button>(); //NPCTextBoard에 버튼 기능을 가져옴
+        btn.onClick.Invoke(); //누름
+
+
 
     }
 }
