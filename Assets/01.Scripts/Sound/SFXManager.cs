c3d539d3f5ae659d5cba7bdce91f1da59a1b6aaf
YuJHun <76084438+YuJHun@users.noreply.github.com>
2025-05-29 13:02:56 +0900

    타이틀씬수정,효과음추가

---
+++ Assets/01.Scripts/Sound/SFXManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class SFXManager : MonoBehaviour
+{
+    [Header("Bear SFX")]
+    public GameObject bearAttackSFX;
+    public GameObject bearDieSFX;
+    public GameObject bearHitSFX;
+
+    [Header("Player SFX")]
+    public GameObject playerAttackSFX;
+    public GameObject playerDieSFX;
+    public GameObject playerHitSFX;
+    public GameObject playerJumpSFX;
+
+    public void PlaySFX(GameObject sfxPrefab, Vector3 position)
+    {
+        if (sfxPrefab == null) return;
+
+        GameObject sfx = Instantiate(sfxPrefab, position, Quaternion.identity);
+        float clipLength = sfx.GetComponent<AudioSource>().clip.length;
+        Destroy(sfx, clipLength);
+
+
+        //사용예시 public SFXMananger sfxMananger
+        //sfxManager.PlaySFX(sfxManager.playerAttackSFX, transform.position);
+    }
+}
